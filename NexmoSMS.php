4150aa6cdf080f8e5a79299bfea37322f40d1625
Kaiyuan Liu <liu.k@gameonmedia.ie>
2015-05-28 16:30:30 +0100

    get rid of helpers function, move http_response_code into NexmoSMS class

--- NexmoSMS.php
+++ NexmoSMS.php
@@ -1,7 +1,4 @@
 <?php
-include __DIR__ . '/helpers.php';
-
-
 /**
  * Implementation of Nexmo SMS API including sending sms message,
  *  receiving inbound message and handling delivery receipt
@@ -343,7 +340,80 @@ class NexmoSMS
         $input = str_replace('?', '', @file_get_contents('php://input'));
         parse_str($input, $receiveDataArray);
         // always send 200 status code to Nexmo
-        http_response_code(200);
+        if (!function_exists('http_response_code')) {
+            self::http_response_code(200);
+        } else {
+            http_response_code(200);
+        }
+    }
+
+    /*
+     * Be compatible with <= php 5.4, Could
+     *
+     * Thanks: http://php.net/http_response_code#107261
+     *
+     * */
+    public static function http_response_code($code = null) {
+
+        if ($code !== null) {
+
+            switch ($code) {
+                case 100: $text = 'Continue'; break;
+                case 101: $text = 'Switching Protocols'; break;
+                case 200: $text = 'OK'; break;
+                case 201: $text = 'Created'; break;
+                case 202: $text = 'Accepted'; break;
+                case 203: $text = 'Non-Authoritative Information'; break;
+                case 204: $text = 'No Content'; break;
+                case 205: $text = 'Reset Content'; break;
+                case 206: $text = 'Partial Content'; break;
+                case 300: $text = 'Multiple Choices'; break;
+                case 301: $text = 'Moved Permanently'; break;
+                case 302: $text = 'Moved Temporarily'; break;
+                case 303: $text = 'See Other'; break;
+                case 304: $text = 'Not Modified'; break;
+                case 305: $text = 'Use Proxy'; break;
+                case 400: $text = 'Bad Request'; break;
+                case 401: $text = 'Unauthorized'; break;
+                case 402: $text = 'Payment Required'; break;
+                case 403: $text = 'Forbidden'; break;
+                case 404: $text = 'Not Found'; break;
+                case 405: $text = 'Method Not Allowed'; break;
+                case 406: $text = 'Not Acceptable'; break;
+                case 407: $text = 'Proxy Authentication Required'; break;
+                case 408: $text = 'Request Time-out'; break;
+                case 409: $text = 'Conflict'; break;
+                case 410: $text = 'Gone'; break;
+                case 411: $text = 'Length Required'; break;
+                case 412: $text = 'Precondition Failed'; break;
+                case 413: $text = 'Request Entity Too Large'; break;
+                case 414: $text = 'Request-URI Too Large'; break;
+                case 415: $text = 'Unsupported Media Type'; break;
+                case 500: $text = 'Internal Server Error'; break;
+                case 501: $text = 'Not Implemented'; break;
+                case 502: $text = 'Bad Gateway'; break;
+                case 503: $text = 'Service Unavailable'; break;
+                case 504: $text = 'Gateway Time-out'; break;
+                case 505: $text = 'HTTP Version not supported'; break;
+                default:
+                    exit('Unknown http status code "' . htmlentities($code) . '"');
+                    break;
+            }
+
+            $protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
+
+            header($protocol . ' ' . $code . ' ' . $text);
+
+            $GLOBALS['http_response_code'] = $code;
+
+        } else {
+
+            $code = (isset($GLOBALS['http_response_code']) ? $GLOBALS['http_response_code'] : 200);
+
+        }
+
+        return $code;
+
     }
 
     public function logSMS(array $dbDetails, array $messageDetails)
